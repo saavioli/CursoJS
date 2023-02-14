596e87069417a779b18ccf3dc0e45ce2af10eeb2
Igor Savioli <igortestasavioli@hotmail.com>
2023-02-14 10:17:14 -0300

    Adiçoes de exercícios

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 10;
+
+if (idade < 16){
+    console.log('Não vota.')
+} else if (idade < 18 || idade >= 65) {
+    console.log('Voto não obrigatório')
+} else
+    console.log('Voto obrigatório')
